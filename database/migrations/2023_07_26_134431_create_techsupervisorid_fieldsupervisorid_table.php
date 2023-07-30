fbe1dacb0e9b47929c69adabd4308b75d10cfaec
Jana <393o9a15@gmail.com>
2023-07-30 14:01:20 +0300

    first commit

---
+++ database/migrations/2023_07_26_134431_create_techsupervisorid_fieldsupervisorid_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTechsupervisoridFieldsupervisoridTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('techsupervisorid_fieldsupervisorid', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('techsupervisorid_fieldsupervisorid');
+    }
+}
