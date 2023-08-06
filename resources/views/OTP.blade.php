93ce9f940b32bb85a5e1a8d220f51a28882dc551
Jana <393o9a15@gmail.com>
2023-08-06 15:00:27 +0300

    OTP

---
+++ resources/views/OTP.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8"> <title>OTP Password</title>
+    </head>
+
+    <body>
+        <p> This is your one time password, if you did not request this email please just egnore it </p>
+
+        {{$token}}
+
+<p> Thank You For Contacting us ..<br> Best Regards ..<br> Moshref Team<br>
+</p>
+
+    </body>
+</html>
